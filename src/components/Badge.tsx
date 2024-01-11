3ff76916dd9bfb4a2965c84ef7c67590fe612af8
DESKTOP-DK0BURO\Alexis <alexis.chapellier@gmail.com>
2024-01-11 02:52:47 +0100

    1

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -50,17 +50,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      By
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://erepare33.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> E.repare AI</span>
       </a>
     </span>
   );
